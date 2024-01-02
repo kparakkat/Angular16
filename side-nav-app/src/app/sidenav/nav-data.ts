52d8b199250ae50934c449edb780cf7b75267f75
kparakkat <53283195+kparakkat@users.noreply.github.com>
2024-01-01 19:35:31 -0500

    Navbar Updates

--- side-nav-app/src/app/sidenav/nav-data.ts
+++ side-nav-app/src/app/sidenav/nav-data.ts
@@ -10,81 +10,81 @@ export const navbarData: INavbarData[] = [
         routeLink: 'products',
         icon: 'fal fa-box-open',
         label: 'Products',
-        items: [
-            {
-                routeLink: 'products/level1.1',
-                label: 'Level 1.1',
-                items: [
-                    {
-                        routeLink: 'products/level2.1',
-                        label: 'Level 2.1',
-                    },
-                    {
-                        routeLink: 'products/level2.2',
-                        label: 'Level 2.2',
-                        items: [
-                            {
-                                routeLink: 'products/level3.1',
-                                label: 'Level 3.1'
-                            },
-                            {
-                                routeLink: 'products/level3.2',
-                                label: 'Level 3.2'
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                routeLink: 'products/level1.2',
-                label: 'Level 1.2',
-            }
-        ]
+        // items: [
+        //     {
+        //         routeLink: 'products/level1.1',
+        //         label: 'Level 1.1',
+        //         items: [
+        //             {
+        //                 routeLink: 'products/level2.1',
+        //                 label: 'Level 2.1',
+        //             },
+        //             {
+        //                 routeLink: 'products/level2.2',
+        //                 label: 'Level 2.2',
+        //                 items: [
+        //                     {
+        //                         routeLink: 'products/level3.1',
+        //                         label: 'Level 3.1'
+        //                     },
+        //                     {
+        //                         routeLink: 'products/level3.2',
+        //                         label: 'Level 3.2'
+        //                     }
+        //                 ]
+        //             }
+        //         ]
+        //     },
+        //     {
+        //         routeLink: 'products/level1.2',
+        //         label: 'Level 1.2',
+        //     }
+        // ]
     },
     {
         routeLink: 'statistics',
-        icon: 'fal fa-chart-bar',
-        label: 'Statistics'
+        icon: 'fal fa-scanner-touchscreen',
+        label: 'Status'
     },
     {
         routeLink: 'coupens',
-        icon: 'fal fa-tags',
-        label: 'Coupens',
-        items: [
-            {
-                routeLink: 'coupens/list',
-                label: 'List Coupens'
-            },
-            {
-                routeLink: 'coupens/create',
-                label: 'Create Coupens'
-            }
-        ]
+        icon: 'fal fa-comments',
+        label: 'Comments',
+        // items: [
+        //     {
+        //         routeLink: 'coupens/list',
+        //         label: 'List Coupens'
+        //     },
+        //     {
+        //         routeLink: 'coupens/create',
+        //         label: 'Create Coupens'
+        //     }
+        // ]
     },
     {
         routeLink: 'pages',
         icon: 'fal fa-file',
-        label: 'Pages'
+        label: 'Documents'
     },
-    {
-        routeLink: 'media',
-        icon: 'fal fa-camera',
-        label: 'Media'
-    },
+    // {
+    //     routeLink: 'media',
+    //     icon: 'fal fa-camera',
+    //     label: 'Media'
+    // },
     {
         routeLink: 'settings',
         icon: 'fal fa-cog',
         label: 'Settings',
-        expanded: true,
-        items: [
-            {
-                routeLink: 'settings/profile',
-                label: 'Profile'
-            },
-            {
-                routeLink: 'settings/customize',
-                label: 'Customize'
-            }
-        ]
+        // expanded: true,
+        // items: [
+        //     {
+        //         routeLink: 'settings/profile',
+        //         label: 'Profile'
+        //     },
+        //     {
+        //         routeLink: 'settings/customize',
+        //         label: 'Customize'
+        //     }
+        // ]
     },
 ];
